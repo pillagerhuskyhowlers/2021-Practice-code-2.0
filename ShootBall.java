03b8626a2a0de1f0367d4780bd4fd88eed67a9b3
pillagerhuskyhowlers <81455947+pillagerhuskyhowlers@users.noreply.github.com>
2021-03-30 11:42:06 -0500

    Add files via upload

---
+++ ShootBall.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import frc.robot.subsystems.Shooter;
+
+public class ShootBall {
+
+	public ShootBall(Shooter shooter) {
+	}
+
+	public void addRequirements(Shooter shooter) {
+	}
+
+}
